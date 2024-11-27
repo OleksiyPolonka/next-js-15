a1117aa4451c1ddaf2a91dabdffb8722b4727cc7
oleksii.polonka <olpolonka@car-fluent.com>
2024-11-28 00:42:27 +0200

    add clerk auth

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,12 +1,13 @@
 'use client'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 export const Navigation = () => {
   const pathName = usePathname()
-console.log('%csrc/app/components/navigation.tsx:7 pathName', 'color: #007acc;', pathName);
+
   return (
-    <nav>
+    <nav className="flex justify-center items-center p-4">
       <Link
         href="/"
         className={pathName === '/' ? 'mr-4 text-white-500' : 'mr-4 text-blue-400'}
@@ -25,6 +26,14 @@ console.log('%csrc/app/components/navigation.tsx:7 pathName', 'color: #007acc;',
       >
         Product 1
       </Link>
+
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </nav>
   )
 }
